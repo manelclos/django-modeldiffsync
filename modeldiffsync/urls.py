f561757576c6342d5d4e38e14d14d6649d9b9cf1
Manel Clos <manelclos@gmail.com>
2020-01-25 07:53:28 +0100

    delete all files

--- modeldiffsync/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from modeldiffsync.api import GeomodeldiffList, Update
-
-
-urlpatterns = [
-    url(r'^geomodeldiff$', GeomodeldiffList.as_view()),
-    url(r'^update', Update.as_view()),
-]
--- run_modeldiffsync.py
+++
@@ -1,6 +0,0 @@
-from modeldiffsync.models import ModeldiffSync
-from modeldiffsync.utils import run_sync
-
-
-for sync in ModeldiffSync.objects.all():
-    run_sync(sync)
